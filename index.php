7118e5fae928fcb9900c5ae876d3efc0418d936b
AbaliHorty <abalibezahortance@gmail.com>
2025-11-05 11:56:28 +0200

    index page is created

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>student mis</title>
+</head>
+<body>
+    <h3>Welcome to students MIS</h3>
+    
+    <p>Don't have an account? <a href="registration.php">Register here</a></p>
+</body>
+</html>
+
+?>
